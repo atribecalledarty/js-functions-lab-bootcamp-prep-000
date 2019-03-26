a50684cfab738d476e41ee1ed6dd30ba11297aa8
Arthur Song <arth3rs0ng@gmail.com>
2019-03-26 03:57:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return 'Happy holidays, ' + name + '!'
+  return 'Happy holidays, ${name}'
 }
 
+/*
+function happyHolidaysTo(name){
+  return 'Happy holidays, ' + name + '!'
+}
+*/
 function happyHolidayTo(holiday, name){
   return 'Happy ' + holiday + ', ' + name + '!'
 }
